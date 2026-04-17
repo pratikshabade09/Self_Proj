0c3e84960a12bb08947e43e99bc0e8a5a4ce0da8
pratikshabade09 <badepratiksha37@gmail.com>
2026-04-18 00:13:47 +0530

    .

--- server.js
+++ server.js
@@ -1,16 +1,25 @@
 const express =require('express');
-const app = express();
-app.use(express.json());    //middleware
+const app = express();  //this creates actual server(instance)
+app.use(express.json());    //middleware (json to JS obj) eg, when we want data by 'req.body'
 
-app.get('/',(req,res)=>{
+app.get('/',(req,res)=>{    //wht to send to user when it reached for certain route
     res.send('Server runnig');
 });
 
 const port = 3000;
-app.listen(port,()=>{
+app.listen(port,()=>{       //listens requests continuously
     console.log(`Server started at port ${port}`)
 })
 
-app.post('/login',(req,res)=>{
-    const {username,password} = req.body;
+app.post('/login',(req,res)=>{          //used post here bcz, login ===security
+    const {username,password} = req.body;   //extract data from client
+    console.log(username,password);
+    res.send("--------------------Secure login------------------")
 })
+
+//get -> visible in url 
+//post -> not visible in url (secure)
+
+
+const mysql = require('mysql2');
+
